fdfc22aefea4745a2ef114529ecd3c8063565b35
Flavia Lisboa <flavia@ifsc.usp.br>
2018-08-24 15:08:57 -0300

    Issue 4 - criando disciplinas

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,4 +1,5 @@
     <h1>Disciplinas</h1>
+    <a href="/disciplinas/create">Cadastrar nova disciplina</a>
     <ul>
     @foreach ($disciplinas as $disciplina)
         <li><a href="/disciplinas/{{ $disciplina->id }}">
