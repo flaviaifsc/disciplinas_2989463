6cfbe6d18b165a59a37e7ecf10dbfb0ca826ac0c
Flavia Lisboa <flavia@ifsc.usp.br>
2018-08-26 16:31:39 -0300

    Issue10 - Mostrando turmas nas disciplinas

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -1,3 +1,10 @@
 <h1>{{ $disciplina->titulo }}</h1>
 <p>{{ $disciplina->ementa }}</p>
+<h2>Turmas:</h2>
+@foreach ($disciplina->turmas as $turma)
+        {{ $turma->ministrante }}
+        {{ $turma->inicio }}
+<br>
+@endforeach
+<br>
 <a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Inserir Turma</a>
