b6738bed19c1290f1355f11b73298249ddea6391
Flavia Lisboa <flavia@ifsc.usp.br>
2018-08-26 16:27:38 -0300

    Issue 9 - cadastrando turmas em disciplinas

--- app/Http/Controllers/DisciplinaController.php
+++ app/Http/Controllers/DisciplinaController.php
@@ -63,7 +63,7 @@ class DisciplinaController extends Controller
      */
     public function edit(Disciplina $disciplina)
     {
-        //
+        return view('disciplinas.edit',compact('disciplina'));
     }
 
     /**
@@ -75,7 +75,11 @@ class DisciplinaController extends Controller
      */
     public function update(Request $request, Disciplina $disciplina)
     {
-        //
+        $disciplina->titulo = $request->titulo;
+        $disciplina->ementa = $request->ementa;
+        $disciplina->save();
+
+        return redirect("/disciplinas/$disciplina->id");
     }
 
     /**
@@ -86,6 +90,24 @@ class DisciplinaController extends Controller
      */
     public function destroy(Disciplina $disciplina)
     {
-        //
+        $disciplina->delete();
+        return redirect('/');
     }
+
+    public function createTurma($disciplina_id)
+    {
+        return view('disciplinas.turmas.create',compact('disciplina_id'));
+    }
+
+    public function storeTurma(Request $request, $disciplina_id)
+    {
+        $turma = new \App\Turma;
+        $turma->ministrante = $request->ministrante;
+        $turma->inicio = $request->inicio;
+        $turma->fim = $request->fim;
+        $turma->bibliografia = $request->bibliografia;
+        $turma->disciplina_id = $request->disciplina_id;
+        Disciplina::find($disciplina_id)->turmas()->save($turma);
+        return redirect("/disciplinas/$disciplina_id");
+    }    
 }
---
+++ app/Turma.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Turma extends Model
+{
+    public function disciplina()
+	{
+    return $this->belongsTo('App\Disciplina');
+	}
+}
+
